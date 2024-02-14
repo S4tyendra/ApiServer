e1799c5121d29dc946c0c74b663c58d87fc53314
Satyendra <satya@satyendra.in>
2024-02-14 11:58:50 +0530

    Add HTML form routes for OTP verification and login page

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import time
 
 import aiofiles
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse
 from pyrogram import Client
 
 from api.countrystatesapi import router as countrystates_router
 from auth.login import router as auth_router
+from database import connect_to_database
 from user.profile import router as user_router
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s',
@@ -62,3 +64,91 @@ if os.path.exists(".env"):
 app.include_router(auth_router, tags=["auth"], prefix="/auth")
 app.include_router(user_router, tags=["user"], prefix="/user")
 app.include_router(countrystates_router, tags=["World cities api", ], prefix="/api")
+
+
+login_page_html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="/auth/login-post" method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+"""
+
+home_page_html = """
+<h1>API Keys</h1>
+<button onclick="logout()">Logout</button>
+<ul id="api-keys-list">
+    <!-- API keys will be dynamically added here -->
+</ul>
+<button onclick="generateApiKey()">Generate API Key</button>
+<button onclick="deleteAllKeys()">Delete All Keys</button>
+<script>
+// Fetch API keys from the server /auth/listapikeys
+fetch("/auth/listapikeys")
+.then(response => response.json())
+.then(data => {
+    const apiKeys  = data.api_keys;
+    const apiKeysList = document.getElementById("api-keys-list");
+    apiKeys.forEach(apiKey => {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(apiKey));
+        apiKeysList.appendChild(li);
+    });
+});
+function copyKey(key) {
+    //window.navigator.clipboard.writeText(key);
+    //# alert("API Key copied to clipboard");
+}
+    
+    
+function deleteAllKeys(){
+    fetch("/auth/deleteapikeys").then(
+        window.reload();
+    )
+}
+    
+    
+    
+function generateApiKey() {
+    fetch("/auth/createapikey")
+    .then(response => response.json())
+    .then(data => {
+        const apiKey = data.api_key;
+        const apiKeysList = document.getElementById("api-keys-list");
+        const li = document.createElement("li");
+        innerhtml = `<pre>${apiKey}</pre> <button onclick="copyKey('${apiKey}')">COPY</button>`;
+        li.innerHTML = innerhtml;
+        apiKeysList.appendChild(li);
+        //copy to clipboard
+        //alert("API Key:"+apiKey+" is copied to clipboard");
+        //navigator.clipboard.writeText(apiKey);
+    });
+}
+</script>
+"""
+
+
+@app.get("/")
+async def root(request: Request, response: Response):
+    cookie = request.cookies.get("_id-c")
+    if cookie is None:
+        response.status_code = 401
+        return HTMLResponse(content=login_page_html, status_code=401)
+    if cookie:
+        db = await connect_to_database()
+        cookie_user = await db.sessions.find_one({"_id": cookie})
+        if cookie_user is None:
+            response.status_code = 401
+            return HTMLResponse(content=login_page_html, status_code=401)
+        cookie_user_email = cookie_user.get("email")
+        return HTMLResponse(content=home_page_html, status_code=200)
