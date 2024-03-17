4d7eef36d13b233f4ee64c3c776eb6c0e45974e3
Satyendra <satya@devh.in>
2024-03-17 06:25:18 +0000

    RB

--- main.py
+++ main.py
@@ -347,13 +347,31 @@ async def root_post():
 
 
 def convert_to_pdf(mdc,title):
-    from markdown_pdf import MarkdownPdf
-    from markdown_pdf import Section
-    pdf = MarkdownPdf(toc_level=1)
-    pdf.add_section(Section(mdc))
-    pdf.meta["title"] = title
-    pdf.save(f"temp/{title}.pdf")
-    return f"temp/{title}.pdf"
+   # from markdown_pdf import MarkdownPdf
+   # from markdown_pdf import Section
+   # pdf = MarkdownPdf(toc_level=1)
+   # pdf.add_section(Section(mdc))
+   # pdf.meta["title"] = title
+   # pdf.save(f"temp/{title}.pdf")
+   # return f"temp/{title}.pdf"
+   from md2pdf.core import md2pdf
+   md2pdf(
+    pdf=f"temp/{title}.pdf",
+    raw = mdc,
+    css = "font.css",
+    extras = [
+    "markdown.extensions.tables",
+    "markdown.extensions.codehilite",
+    "pymdownx.magiclink",
+    "pymdownx.betterem",
+    "pymdownx.superfences",
+    "pymdownx.highlight",
+    "pymdownx.snippets",
+    "markdown.extensions.wikilinks",
+    "markdown.extensions.toc",
+]
+)
+   return f"temp/{title}.pdf"
 
 async def delete_file(file_path: str):
     try:
@@ -366,5 +384,4 @@ async def send_pdf(request: Request, mdc=Form(...), title=Form(...)):
     file_name = convert_to_pdf(mdc, title)
     response = FileResponse(file_name, media_type="application/pdf", filename=file_name)
     return response
-
 
