b3bf7df3bf0a9d10798892470dc193973574f4fd
Ubuntu <ubuntu@ip-172-31-37-176.ap-south-1.compute.internal>
2024-03-27 05:38:08 +0000

    Update

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from xml.dom.minidom import DocumentType
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # MongoDB connection URL
-MONGODB_URL = "mongodb+srv://satya:satya@satyavercel.vkbon8d.mongodb.net/?retryWrites=true&w=majority"
+MONGODB_URL ="mongodb+srv://s4tyendra:satya@devh.cbvk0so.mongodb.net/?retryWrites=true&w=majority&appName=devh" # "mongodb+srv://satya:satya@satyavercel.vkbon8d.mongodb.net/?retryWrites=true&w=majority"
 
 async def connect_to_database():
     client = AsyncIOMotorClient(MONGODB_URL)
