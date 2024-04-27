3e6b0da7e3249324969b08223e46e473545e44ca
Satyendra <satya@devh.in>
2024-04-27 17:43:30 +0530

    Fix encryption bug in Google authentication router

--- auth/google.py
+++ auth/google.py
@@ -1,50 +1,85 @@
 import os
-from fastapi import APIRouter, Request, Depends, HTTPException
+import secrets
+from fastapi import APIRouter, Request, Depends, HTTPException, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from google.auth.transport import requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
+from google.oauth2 import id_token as google_id_token
+import base64
+from datetime import datetime
+
+from database import connect_to_database 
 
 router = APIRouter()
 CLIENT_SECRETS_FILE = "auth/clientsecret.json"
 SCOPES = ['https://www.googleapis.com/auth/userinfo.email', 
           'https://www.googleapis.com/auth/userinfo.profile',
           'openid']
+
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+
+red_map = {}
+
 @router.get('/glogin')
-async def login():
+async def login(redirect = None):
     flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, redirect_uri="https://aws-api.devh.in/auth/googlesignin" 
+        CLIENT_SECRETS_FILE, scopes=SCOPES,
+        redirect_uri="http://localhost:8000/auth/googlesignin" 
+        # redirect_uri="https://aws-api.devh.in/auth/googlesignin" 
     )  # Use your FastAPI server's callback URL
     authorization_url, state = flow.authorization_url(
         access_type='offline', include_granted_scopes='true'
     )
+    red_map[state] = redirect
     return RedirectResponse(authorization_url)
 
-from google.oauth2 import id_token as google_id_token
-import base64
+
 
 @router.get('/googlesignin')
-async def callback(request: Request):
+async def callback(request: Request, response: Response):
     state = request.query_params.get('state')  # Extract state parameter
-    redirect_uri = "https://aws-api.devh.in/auth/googlesignin"  # Update with your FastAPI server's callback URL
-    flow = Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state, redirect_uri=redirect_uri
-    )
+    if state in red_map:
+        redirect_uri="http://localhost:8000/auth/googlesignin" 
+        #redirect_uri = "https://aws-api.devh.in/auth/googlesignin"  # Update with your FastAPI server's callback URL
+        flow = Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE, scopes=SCOPES, state=state, redirect_uri=redirect_uri
+        )
 
-    flow.fetch_token(authorization_response=str(request.url).replace("http", "https"))
+        flow.fetch_token(authorization_response=str(request.url).replace("http", "https"))
 
-    id_token_data = google_id_token.verify_oauth2_token(flow.credentials.id_token, requests.Request())
+        id_token_data = google_id_token.verify_oauth2_token(flow.credentials.id_token, requests.Request())
 
-    # Extract user data
-    email = id_token_data['email']
-    name = id_token_data['name']
-    picture = id_token_data['picture']
-
-    data = xor_encrypt(str({"email": email, "name": name, "picture": picture}), key ="x7Saty@3")
+        # Extract user data
+        email = id_token_data['email']
+        name = id_token_data['name']
+        picture = id_token_data['picture']
+        
+        db = await connect_to_database()
+        user = await db.users.find_one({"email": email})
+        cookie = secrets.token_hex(32)
+        
+        if user is None:
+            id = str(datetime.now().timestamp()).replace(".", "")
+            await db.users.insert_one({"_id":id,"email": email, "name": name, "picture": picture, "role": "user"})
+            await db.sessions.insert_one(
+                        {"_id": cookie, "email": email, "created_at": datetime.now().timestamp()})
+            response.set_cookie(key="_id-c", value=cookie, httponly=False, secure=False)
+        else:
+            id = user["_id"]
+            await db.sessions.update_one({"email": email}, {"$set": {"created_at": datetime.now().timestamp()}})
+            response.set_cookie(key="_id-c", value=cookie, httponly=False, secure=False)
+        
+        red_url = red_map.get(state)
+        if red_url is not None:
+            del red_map[state]
+            return RedirectResponse(f"{red_url}?token={cookie}")
+        return RedirectResponse("/")
+        
+    else:
+        raise HTTPException(status_code=400, detail="Invalid state")
     
-    return RedirectResponse(f"https://iiitk.devh.in/auth?token={data}")
     
     
 def xor_encrypt(data, key):
@@ -53,4 +88,3 @@ def xor_encrypt(data, key):
         encrypted_data += chr(ord(data[i]) ^ ord(key[i % len(key)]))
     return base64.b64encode(encrypted_data.encode()).decode()
 
-xor_encrypt(str({"email": "email", "name": "name", "picture": "picture"}), key ="x7Saty@3")
