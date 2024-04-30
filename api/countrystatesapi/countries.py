7241b683adf1f2efe5ca7076d6586bb47fa806ad
Satyendra <satya@devh.in>
2024-04-30 10:31:13 +0530

    Refactor database connection function names in datab.py and countries.py

--- api/countrystatesapi/countries.py
+++ api/countrystatesapi/countries.py
@@ -3,14 +3,21 @@ from fastapi.responses import FileResponse
 from fastapi import APIRouter, HTTPException, Depends
 from functions.apiwrapper import api_key_auth
 
-from api.countrystatesapi.datab import connect_to_database
+from api.countrystatesapi.datab import world_db
+from fastapi import APIRouter, HTTPException, Depends
+from functions.apiwrapper import api_key_auth
+from api.countrystatesapi.datab import world_db
+from bson import ObjectId
 router = APIRouter()
 
 
+router = APIRouter()
+
 @router.get("/countrieslist", dependencies=[Depends(api_key_auth)])
 async def get_countries():
-        db = await connect_to_database()
-        countries = await db.countries.find()
-        print(countries)
-        return FileResponse('assets/countries.json', media_type='application/json')
+        db = await world_db()
+        countries = db.countries.find()
+        country_list = [country async for country in countries if '_id' in country]
+        country_list = [{**country, '_id': str(country['_id'])} for country in country_list]
+        return country_list
 
