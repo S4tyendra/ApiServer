84725b24edb8bf20590866086ab70f3675f5dc5c
Satyendra <satya@devh.in>
2024-04-30 10:01:25 +0530

    Remove unused JSON files for Kenya, Malta, and Meru from assets folder

--- api/countrystatesapi/countries.py
+++ api/countrystatesapi/countries.py
@@ -2,10 +2,15 @@ from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from fastapi import APIRouter, HTTPException, Depends
 from functions.apiwrapper import api_key_auth
+
+from api.countrystatesapi.datab import connect_to_database
 router = APIRouter()
 
 
 @router.get("/countrieslist", dependencies=[Depends(api_key_auth)])
 async def get_countries():
+        db = await connect_to_database()
+        countries = await db.countries.find()
+        print(countries)
         return FileResponse('assets/countries.json', media_type='application/json')
 
