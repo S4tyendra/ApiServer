8417894f889aa91f9738fffd104dcc6d1b30ca08
Satyendra <satya@devh.in>
2024-08-08 16:54:44 +0530

    chore: Refactor code to remove unused database connection function and update notes insertion logic

--- database.py
+++ database.py
@@ -7,9 +7,9 @@ MONGODB_URL = "mongodb+srv://s4tyendra:satya@devh.cbvk0so.mongodb.net/?retryWrit
 # "mongodb+srv://satya:satya@satyavercel.vkbon8d.mongodb.net/?retryWrites=true&w=majority"
 NOTES_DB_URL="mongodb+srv://mongodb:satyendra@mongodbdevh.9fqlqam.mongodb.net/?retryWrites=true&w=majority&appName=mongodbdevh"
 
-async def connect_to_database():
+async def connect_to_database(db_name = None):
     client = AsyncIOMotorClient(MONGODB_URL)
-    db = client["fastapi_users_db"]
+    db = client[db_name or "fastapi_users_db"]
     return db
 
 async def connect_to_notes_database():
