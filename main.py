147c5e511770e18c1965c5ff3d3d3bfe65d0716a
satya <satya@devh.in>
2024-10-02 01:10:59 +0530

    Include cdn for css to save bandwidth

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ async def health_check():
     <head>
         <title>Server Health Check Dashboard</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="static/all.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
         <style>
             :root {{
