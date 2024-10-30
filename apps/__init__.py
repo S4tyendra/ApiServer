5fbf8be1d39c11d1260747102e66b396dc9b7c8d
Satyendra <satya@devh.in>
2024-10-30 16:48:08 +0530

    Addeed

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,5 +5,7 @@ router = APIRouter()
 
 from .listapps import router as listapps_router
 from .detail import router as detail_router
+from .approve import router as approve_router
 router.include_router(listapps_router)
 router.include_router(detail_router)
+router.include_router(approve_router)
