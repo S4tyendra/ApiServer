6141fcce6ba1f7dab33093c22a33666c94c4b3cc
s4tyendra <satya@satyendra.in>
2024-10-30 13:18:10 +0530

    idk

---
+++ apps/detail.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException, Request, Response, Depends
+from typing import List, Dict, Any
+from functions.apiwrapper import get_user
+from functions.db import get_database
+
+router = APIRouter()
+
+
+@router.get("/detail")
+async def get_app_detail(request: Request, response: Response, app:str):
+    user = await get_user(request, accept=["WEB-KEY"])
+    db = await get_database()
+    sessions = db.sessions.find({"email": user.get("email"),"type":app})
+    app_detail = await db.apps.find_one({"_id":app})
+    if not app_detail:
+        response.status_code = 404
+        raise HTTPException(status_code=404, detail="App not found")
+    sessions_tr = []
+    async for session in sessions:
+        sessions_tr.append(session)
+    return {"sessions": sessions_tr, **app_detail}
