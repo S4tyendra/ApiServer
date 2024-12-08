a5d980a7b0eff7fce0d49235febc5a347efd5430
s4tyendra <satya@satyendra.in>
2024-12-08 15:30:35 +0530

    2 sec?

--- restart_server.sh
+++ restart_server.sh
@@ -3,8 +3,5 @@
 # Kill the existing hypercorn process
 pkill -f "hypercorn main:app"
 
-# Wait a moment for the process to terminate
-sleep 2
-
 # Start the server again
 hypercorn main:app -b localhost:5001 > server.log 2>&1 &
